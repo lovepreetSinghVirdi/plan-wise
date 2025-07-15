16c23f43eba0c2625f56dff0a076836b8b0f8cf0
lovepreet-singh-virdi <lovepreetsinghvirdi001@gmail.com>
2025-07-15 18:50:38 -0400

    dummy comment

--- src/Components/AppHeader.jsx
+++ src/Components/AppHeader.jsx
@@ -23,7 +23,7 @@ export default function AppHeader() {
         <Collapse isOpen={isOpen} navbar>
           <Nav className="ms-auto" navbar>
             <NavItem>
-              <NavLink href="#homepage">Home</NavLink>
+              <NavLink href="#home">Home</NavLink>
             </NavItem>
             <NavItem>
               <NavLink href="#about">About</NavLink>
