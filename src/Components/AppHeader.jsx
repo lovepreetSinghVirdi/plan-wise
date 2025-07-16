6f269e52071218e90b7f63501a8273520555e028
lovepreet-singh-virdi <lovepreetsinghvirdi001@gmail.com>
2025-07-16 11:57:41 -0400

    replaced bootstrap and reactstrap with material-ui

--- src/Components/AppHeader.jsx
+++ src/Components/AppHeader.jsx
@@ -1,39 +1,98 @@
-import { useState } from 'react';
-import {
-  Navbar,
-  NavbarBrand,
-  NavbarToggler,
-  Collapse,
-  Nav,
-  NavItem,
-  NavLink,
-  Container
-} from 'reactstrap';
+import React, { useState } from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import MenuIcon from '@mui/icons-material/Menu';
+import Button from '@mui/material/Button';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
 
 export default function AppHeader() {
-  const [isOpen, setIsOpen] = useState(false);
-  const toggle = () => setIsOpen(!isOpen);
-  const appName = 'PlanWise'
+  const [mobileOpen, setMobileOpen] = useState(false);
+  const appName = 'PlanWise';
+
+  const navItems = [
+    { label: 'Home', href: '#home' },
+    { label: 'Rogers', href: '#rogers' },
+    { label: 'Bell', href: '#bell' },
+    { label: 'Vmedia', href: '#vmedia' },
+    { label: 'Dodo', href: '#dodo' },
+    { label: 'About Us', href: '#about' },
+  ];
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prev) => !prev);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        {appName}
+      </Typography>
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item.label} disablePadding>
+            <ListItemButton component="a" href={item.href} sx={{ textAlign: 'center' }}>
+              <ListItemText primary={item.label} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
 
   return (
-    <Navbar dark color='dark' expand="md" className="app-navbar sticky-top">
-      <Container className='align-items-center' fluid>
-        <NavbarBrand href="#">{appName}</NavbarBrand>
-        <NavbarToggler onClick={toggle} />
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="sticky" color="primary">
+        <Toolbar>
+          {/* Mobile menu button */}
+          <IconButton
+            color="inherit"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
 
-        {/* Collapse becomes a flex item too */}
-        <Collapse isOpen={isOpen} navbar>
-          {/* ms-auto pushes this Nav to the right */}
-          <Nav className="ms-auto" navbar>
-            <NavItem><NavLink href="#home">Home</NavLink></NavItem>
-            <NavItem><NavLink href="#rogers">Rogers</NavLink></NavItem>
-            <NavItem><NavLink href="#bell">Bell</NavLink></NavItem>
-            <NavItem><NavLink href="#vmedia">Vmedia</NavLink></NavItem>
-            <NavItem><NavLink href="#dodo">Dodo</NavLink></NavItem>
-            <NavItem><NavLink href="#about">About Us</NavLink></NavItem>
-          </Nav>
-        </Collapse>
-      </Container>
-    </Navbar>
+          {/* App name / brand */}
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, textAlign: { xs: 'center', sm: 'left' } }}
+          >
+            {appName}
+          </Typography>
+
+          {/* Desktop nav items */}
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            {navItems.map((item) => (
+              <Button key={item.label} color="inherit" href={item.href}>
+                {item.label}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+
+      {/* Mobile drawer */}
+      <Drawer
+        variant="temporary"
+        open={mobileOpen}
+        onClose={handleDrawerToggle}
+        ModalProps={{ keepMounted: true }} // Better performance on mobile
+        sx={{
+          display: { xs: 'block', sm: 'none' },
+          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: 240 },
+        }}
+      >
+        {drawer}
+      </Drawer>
+    </Box>
   );
 }
