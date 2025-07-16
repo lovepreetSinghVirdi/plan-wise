fc61adf7378c4b7ea77cb59a8fe31908c73b55cf
lovepreet-singh-virdi <lovepreetsinghvirdi001@gmail.com>
2025-07-16 12:34:49 -0400

    code refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
-import Homepage from './Components/Homepage';
 import AppHeader from './Components/AppHeader';
+import HomePage from './Components/Homepage';
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom';
+
 import './App.css'
 
-function App() {
 
+// import RogersPage from './pages/RogersPage';
+// import BellPage   from './pages/BellPage';
+// import VmediaPage from './pages/VmediaPage';
+// import DodoPage   from './pages/DodoPage';
+// import AboutPage  from './pages/AboutPage';
+
+const App = () => {
   return (
-    <>
+    <BrowserRouter>
+      {/* persist your header/nav across all pages */}
       <AppHeader />
-      <Homepage />
-    </>
-  )
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* <Route path="rogers"   element={<RogersPage />} />
+        <Route path="bell"     element={<BellPage />} />
+        <Route path="vmedia"   element={<VmediaPage />} />
+        <Route path="dodo"     element={<DodoPage />} />
+        <Route path="about"    element={<AboutPage />} /> */}
+
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 export default App;
